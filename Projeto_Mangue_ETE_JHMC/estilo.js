4cad20c769be983b1d5f0e7e9171062e5ecb2ba8
CezarDamasceno <juliocezardamascenosousa4@gmail.com>
2025-09-02 21:48:17 -0300

    Delete Projeto_Mangue_ETE_JHMC directory

--- Projeto_Mangue_ETE_JHMC/estilo.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-  const header = document.querySelector('.cabecalho');
-  if (window.scrollY > 50) {
-    header.classList.add('transparente');
-  } else {
-    header.classList.remove('transparente');
-  }
-});
